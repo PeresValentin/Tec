94803848e7869bc6aea97af8e19684bd55948985
Valentin <peresvalentin09@gmail.com>
2024-05-02 14:12:04 -0300

    Ejercicio java

---
+++ EjercicioJava/codigo.js
@@ -0,0 +1,28 @@
+var mensaje = "Hola mundo, que facil es incluir comillas ' ' y comillas \"2\" ";
+alert(mensaje);
+alert("Yo el segundo");
+// Simplemente llama a las alertas 
+
+// Crear el array con los nombres de los meses
+var meses = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+  
+  // Mostrar los nombres de los meses utilizando la función alert()
+//  for (var i = 0; i < meses.length; i++) {
+ //   alert(meses[i]);
+//  }
+//  
+//A partir del siguiente array que se proporciona: var valores = [true, 5, false, "hola", "adios", 2];
+var valores = [true, 5, false, "hola", "adios", 2];
+var auxiliar = 0;
+
+for (var i = 0; i< valores.length; i++) {
+    if ( auxiliar < valores[i].length) {
+        var encontrado = valores[i];
+        auxiliar = valores[i].length
+    }
+}
+
+alert(encontrado);
